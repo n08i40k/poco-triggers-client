045f8ea97687af74f8424729c73ae10d381e0111
N08I40K <nhomcencko1@gmail.com>
2025-04-13 17:52:54 +0400

    1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "ru.n08i40k.poco.triggers"
         minSdk = 33
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.4.1"
+        versionCode = 9
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
