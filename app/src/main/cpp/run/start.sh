73fc1e3c37dc2a66e9dab681045496fe6e481ecd
N08I40K <nhomcencko1@gmail.com>
2025-04-19 20:58:18 +0400

    Testing is not only through usb.

--- app/src/main/cpp/run/start.sh
+++ app/src/main/cpp/run/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-adb -d shell "pkill aosp-triggers-daemon"
-adb -d shell "rm /data/local/tmp/aosp-triggers-daemon*"
-adb -d push aosp-triggers-daemon /data/local/tmp/
-adb -d shell "chmod +x /data/local/tmp/aosp-triggers-daemon"
-adb -d shell "/data/local/tmp/aosp-triggers-daemon"
+adb shell "pkill aosp-triggers-daemon"
+adb shell "rm /data/local/tmp/aosp-triggers-daemon*"
+adb push aosp-triggers-daemon /data/local/tmp/
+adb shell "chmod +x /data/local/tmp/aosp-triggers-daemon"
+adb shell "/data/local/tmp/aosp-triggers-daemon"
